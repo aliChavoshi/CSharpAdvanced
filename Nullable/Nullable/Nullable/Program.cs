9550ba6b6696a07145c3c1400298a1ef4c78b64b
Ali <alichavoshii1372@gmail.com>
2020-11-28 15:00:39 +0330

    Add Custome Exception Project.

--- Nullable/Nullable/Nullable/Program.cs
+++ Nullable/Nullable/Nullable/Program.cs
@@ -6,7 +6,7 @@ namespace Nullable
     {
         static void Main(string[] args)
         {
-            /*#region Exapmle1
+            #region Exapmle1
 
             //Nullable<DateTime> date = null;
             DateTime? date = null;
@@ -21,21 +21,19 @@ namespace Nullable
             DateTime? dateTime = DateTime.Now;
             DateTime dateTime1 = dateTime.GetValueOrDefault();
 
-            #endregion*/
+            #endregion
 
             #region Example3
 
             DateTime? date1 = null;
             DateTime date2 = date1 ?? DateTime.Now;
 
-            /*var date3 = (date1 != null) ? date1.GetValueOrDefault() : DateTime.Now;
-
-
+            var date3 = (date1 != null) ? date1.GetValueOrDefault() : DateTime.Now;
 
             if (date1 != null)
                 date2 = date1.GetValueOrDefault();
             else
-                date2 = DateTime.Now;*/
+                date2 = DateTime.Now;
 
             Console.WriteLine(date2);
 
